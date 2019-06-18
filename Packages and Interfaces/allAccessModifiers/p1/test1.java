a9af09d0c6ed05e5b644db76572efab0d29ee04b
Bhupesh Prajapati <bhupeshprajapati98@gmail.com>
2019-06-18 10:24:49 +0530

    commiting Blockchain implementation and snake and ladder game

--- Packages and Interfaces/allAccessModifiers/p1/test1.java
+++ Packages and Interfaces/allAccessModifiers/p1/test1.java
@@ -6,6 +6,6 @@ public class test1{
     public static void main(String []args){
         Protection ob1=new Protection();
         Derived ob2 =new Derived();
-       p1.SamePackage ob3= SamePackage();
+       //SamePackage ob3= SamePackage();
     }
 }
